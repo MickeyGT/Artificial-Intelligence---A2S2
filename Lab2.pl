a61c3a1b4aab7c80a5ea8f00b8856b2f73058022
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2017-03-28 19:04:55 +0300

    Update Lab2.pl

--- Lab2.pl
+++ Lab2.pl
@@ -1,1 +1,12 @@
 
+meloman(stefan).
+vanator(petre).
+actrita(sotiapetre).
+
+soti(stefan,sotiastef).
+soti(petre,sotiapetre).
+vecini(X,Y):-vecini(Z,Y),soti(X,Z).
+vecini(stefan,petre).
+sentimental(X):-meloman(X).
+mincinos(X):-vanator(X).
+iubeste(X,Y):-actrita(X),sentimental(Y).
